df3905ff9574b5661b4eca712358042099d3e8c7
Evgeniy Shimanogov <e.shimanogov@tinkoffinsurance.ru>
2024-08-05 00:05:52 +0300

    fix

--- update-secret.sh
+++ update-secret.sh
@@ -3,5 +3,5 @@ echo $GH_TOKEN | gh auth login --with-token
 for i in $(gh repo ls | grep boutiqu | awk '{print $1}'); do
 	gh secret -R $i set REPO_NAME  --body "cr.yandex/$(cat out/ycr.conf)"
 	gh secret -R $i set YC_SA_JSON_CREDENTIALS  < out/auth.key.json
-        gh workflow -R $i run workflow.yaml
+        #gh workflow -R $i run workflow.yaml
 done
